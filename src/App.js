2fa8559095c40acf675e6f73c2861a6e0171ccd3
Julio Meza <juliomeza@gmail.com>
2023-09-30 23:33:01 -0400

    Empezando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userMessage, setUserMessage] = useState('');
+  const [chatbotMessage, setChatbotMessage] = useState('');
+
+  const backendUrl = process.env.REACT_APP_BACKEND_URL
+
+  const sendMessage = async (event) => {
+    const response = await fetch(`${backendUrl}/chat`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user_message: userMessage }),
+    });
+
+    const data = await response.json();
+    setChatbotMessage(data.chatbot_response);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={userMessage}
+        onChange={(event) => setUserMessage(event.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+      <p>{chatbotMessage}</p>
     </div>
   );
 }
